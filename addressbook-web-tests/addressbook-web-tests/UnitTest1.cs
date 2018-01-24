5c0135c027765aeefb9b0a9c3777e253ef9fa312
Zagorodskih <azagorodskih@gmail.com>
2018-01-24 17:20:27 +0300

    Лекция 1.4 + частично Лекция 1.5

--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -9,6 +9,20 @@ namespace addressbook_web_tests
         [TestMethod]
         public void TestMethod1()
         {
+            Square s1 = new Square(4);
+            Square s2 = new Square(17);
+            Square s3 = s1;
+
+            //Assert.AreEqual(s1.getSize(), 4);
+            //Assert.AreEqual(s2.getSize(), 17);
+            //Assert.AreEqual(s3.getSize(), 4);
+
+            Assert.AreEqual(s1.Size, 4);
+            Assert.AreEqual(s2.Size, 17);
+            Assert.AreEqual(s3.Size, 4);
+
+            s3.Size = 10;
+            Assert.AreEqual(s1.Size, 10);
         }
     }
 }
