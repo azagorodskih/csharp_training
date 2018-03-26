c25b436c43fb57540560c64b6eed043b3489d318
Zagorodskih <azagorodskih@gmail.com>
2018-03-26 09:34:21 +0300

    Лекция 8.2

---
+++ addressbook_tests_white/addressbook_tests_white/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_white")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("addressbook_tests_white")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29e712bb-e3b1-4a70-ad98-02f144964b65")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_white/addressbook_tests_white/tests/GroupCreationTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace addressbook_tests_white
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        //добавить группу с именем, состоящим из букв
+        public void TestGroupCreation_NameOfLetters()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "white"
+            };
+
+            app.Groups.Add(newGroup);
+            oldGroups.Add(newGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+
+        [Test]
+        //добавить группу с именем, состоящим из цифр
+        public void TestGroupCreation_NameOfDigits()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "67890"
+            };
+
+            app.Groups.Add(newGroup);
+            oldGroups.Add(newGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
