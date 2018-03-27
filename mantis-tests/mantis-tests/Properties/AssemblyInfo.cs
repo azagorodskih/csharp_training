7cc9d28f108d6fa74b4fda1dd9ed2752a79fd539
Zagorodskih <azagorodskih@gmail.com>
2018-03-27 15:40:08 +0300

    Лекция 9.1

---
+++ mantis-tests/mantis-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mantis-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("mantis-tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("848659a3-e33b-48b2-ab58-d7d1a88f5381")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mantis-tests/mantis-tests/appmanager/RegistrationHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace mantis_tests
+{
+    public class RegistrationHelper : HelperBase
+    {
+        public RegistrationHelper(ApplicationManager manager) : base(manager) { }
+
+        public void Register(AccountData account)
+        {
+            OpenMainPage();
+            OpenRegistrationForm();
+            FillRegistrationForm(account);
+            SubmitRegistration();
+        }
+
+        public void OpenMainPage()
+        {
+            manager.Driver.Url = "http://localhost/mantisbt-2.12.0/login_page.php";
+        }
+
+        public void OpenRegistrationForm()
+        {
+            driver.FindElement(By.LinkText("зарегистрировать новую учетную запись")).Click();
+        }
+
+        public void FillRegistrationForm(AccountData account)
+        {
+            driver.FindElement(By.Name("username")).SendKeys(account.Name);
+            driver.FindElement(By.Name("email")).SendKeys(account.Email);
+        }
+
+        public void SubmitRegistration()
+        {
+            driver.FindElement(By.XPath("//input[@value='Зарегистрироваться']")).Click();
+        }
+    }
+}
