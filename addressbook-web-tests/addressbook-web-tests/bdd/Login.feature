cf9618ec85c483af21a3335bb1efc0d3c26e0a6a
Zagorodskih <azagorodskih@gmail.com>
2018-03-29 23:58:58 +0300

    Лекция 10.3

---
+++ addressbook-web-tests/addressbook-web-tests/bdd/Login.feature
@@ -0,0 +1,13 @@
+﻿Feature: Login
+
+Scenario: Login with valid credentials
+	Given A user is logged out
+	#When I login with valid credentials
+	When I login with username "admin" and password "secret"
+	Then I have logged in
+
+Scenario: Login with invalid credentials
+	Given A user is logged out
+	#When I login with invalid credentials
+	When I login with username "admin" and password "12345"
+	Then I have not logged in
---
+++ addressbook-web-tests/addressbook-web-tests/bdd/LoginSteps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [Binding]
+    public class LoginSteps
+    {
+        private ApplicationManager app
+        {
+            get
+            {
+                return ApplicationManager.GetInstance();
+            }
+        }
+
+        [Given(@"A user is logged out")]
+        public void GivenAUserIsLoggedOut()
+        {
+            app.Auth.Logout();
+        }
+
+        //[When(@"I login with valid credentials")]
+        [When(@"I login with username ""(.*)"" and password ""(.*)""")]
+        public void WhenILoginWithValidCredentials(string username, string password)
+        {
+            AccountData account = new AccountData(username, password);
+            ScenarioContext.Current.Add("account", account);
+            app.Auth.Login(account);
+        }
+        
+        //[When(@"I login with invalid credentials")]
+        //public void WhenILoginWithInvalidCredentials()
+        //{
+        //    AccountData account = new AccountData("admin", "12345");
+        //    ScenarioContext.Current.Add("account", account);
+        //    app.Auth.Login(account);
+        //}
+        
+        [Then(@"I have logged in")]
+        public void ThenIHaveLoggedIn()
+        {
+            AccountData account = ScenarioContext.Current.Get<AccountData>("account");
+            Assert.IsTrue(app.Auth.IsLoggedIn(account));
+        }
+        
+        [Then(@"I have not logged in")]
+        public void ThenIHaveNotLoggedIn()
+        {
+            AccountData account = ScenarioContext.Current.Get<AccountData>("account");
+            Assert.IsFalse(app.Auth.IsLoggedIn(account));
+        }
+    }
+}
